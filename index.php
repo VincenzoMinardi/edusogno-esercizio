a15cf1c5719fe7b18ff5f1fadef2a8bfad003e9c
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-10-02 18:10:24 +0200

    add button login and register

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 require_once './assets/db/Connect.php';
 require_once './assets/db/Migration.php';
 
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,10 +13,23 @@ require_once './assets/db/Migration.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/styles/style.css">
+    <script src="./assets/js/script.js" defer></script>
     <title>Edusogno</title>
 </head>
 
 <body>
+    <header>
+        <div class="container">
+            <div>
+                <h1 id="title">Edusogno</h1>
+            </div>
+            <div>
+                <button id="registrazione" class="button">Register</button>
+                <button id="login" class="button">Login</button>
+            </div>
+
+        </div>
+    </header>
 </body>
 
 </html>
