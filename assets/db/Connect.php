66376e64a5037bb5117fc51e3f83c9e37368dcab
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-10-02 16:31:32 +0200

    create database and import date

---
+++ assets/db/Connect.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$host = "localhost"; // Host del database
+$user = "root";           // Nome utente del database
+$password = "root";   // Password del database
+$database = "table_edusogno"; // Nome del database
+
+$connect = new mysqli($host, $user, $password);
+
+if ($connect->connect_error) {
+    die('Errore di connessione: ' . $connect->connect_error);
+}
+
+// Crea il database se non esiste
+$sql = "CREATE DATABASE IF NOT EXISTS $database";
+
+if ($connect->query($sql) === true) {
+    echo "Database creato o esistente con successo<br>";
+} else {
+    echo "Errore nella creazione del database: " . $connect->error . "<br>";
+}
+
+$connect->close();
+
+$connect = new mysqli($host, $user, $password, $database);
+
+if ($connect->connect_error) {
+    die('Errore di connessione al database: ' . $connect->connect_error);
+}
--- index.php
+++ index.php
@@ -1,18 +1,35 @@
+<?php
+// collegamento database mysql alla pagina index
+require_once './assets/db/Connect.php';
+
+// migration dei dati all'interno del database MySQL
+$migrationsFile = './assets/db/Migrations.sql';
+
+$migrationsSql = file_get_contents($migrationsFile);
+
+if (mysqli_multi_query($connect, $migrationsSql)) {
+    do {
+    } while (mysqli_next_result($connect));
+    echo 'Migrazione completata con successo!';
+} else {
+    echo 'Errore nella migrazione: ' . mysqli_error($connect);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/styles/style.css">
     <title>Edusogno</title>
 </head>
 
 <body>
-
-    <h1>ciao a tutti</h1>
-
 </body>
 
 </html>
