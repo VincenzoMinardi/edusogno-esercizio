f770964afdb9f044a10ed1f19632f3b9370964e9
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-10-02 18:10:59 +0200

    popolate form register

--- register.php
+++ register.php
@@ -4,26 +4,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./assets/styles/style.css">
+    <title>Registrazione</title>
 </head>
 
 <body>
-    <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <header>
+        <div class="container">
+            <div>
+                <h1>Edusogno</h1>
+            </div>
+            <div>
+                <button id="registrazione" class="button">Register</button>
+                <button id="login" class="button">Login</button>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+    </header>
+    <main>
+        <!-- Modulo di registrazione -->
+        <div class="container-register">
+            <h2 class="title">Crea il tuo Nuovo Account</h2>
+            <div class="container-form">
+                <form action="process_registration.php" method="POST">
+                    <div class="cont-text">
+                        <label class="text" for="nome">Inserisci il nome:</label>
+                        <input class="input" type="text" id="nome" name="nome" required>
+                    </div>
+                    <div class="cont-text">
+                        <label class="text" for="nome">Inserisci il cognome:</label>
+                        <input class="input" type="text" id="cognome" name="cognome" required>
+                    </div>
+                    <div class="cont-text">
+                        <label class="text" for="nome">Inserisci l'email:</label>
+                        <input class="input" type="email" id="email" name="email" required>
+                    </div>
+                    <div class="cont-text">
+                        <label class="text" for="nome">Inserisci la password:</label>
+                        <input class="input" type="password" id="password" name="password" required>
+                    </div>
+                    <button class="button-register" type="submit"> Registrati</button>
+                </form>
+            </div>
+
         </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </main>
 
 </body>
 
