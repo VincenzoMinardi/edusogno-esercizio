c76d390620e9e908c03a049995c3cd20ffc187ea
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-10-02 16:51:08 +0200

    add migration.php and function duplicate migrate

--- assets/db/Connect.php
+++ assets/db/Connect.php
@@ -1,10 +1,9 @@
 <?php
 
-
-$host = "localhost"; // Host del database
-$user = "root";           // Nome utente del database
-$password = "root";   // Password del database
-$database = "table_edusogno"; // Nome del database
+$host = "localhost:3306";       // Host del database
+$user = "root";                 // Nome utente del database
+$password = "root";             // Password del database
+$database = "table_edusogno";   // Nome del database
 
 $connect = new mysqli($host, $user, $password);
 
@@ -12,7 +11,7 @@ if ($connect->connect_error) {
     die('Errore di connessione: ' . $connect->connect_error);
 }
 
-// Crea il database se non esiste
+// Creazione del Database
 $sql = "CREATE DATABASE IF NOT EXISTS $database";
 
 if ($connect->query($sql) === true) {
--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <?php
 // collegamento database mysql alla pagina index
 require_once './assets/db/Connect.php';
+require_once './assets/db/Migration.php';
 
-// migration dei dati all'interno del database MySQL
-$migrationsFile = './assets/db/Migrations.sql';
-
-$migrationsSql = file_get_contents($migrationsFile);
-
-if (mysqli_multi_query($connect, $migrationsSql)) {
-    do {
-    } while (mysqli_next_result($connect));
-    echo 'Migrazione completata con successo!';
-} else {
-    echo 'Errore nella migrazione: ' . mysqli_error($connect);
-}
 
 ?>
 
