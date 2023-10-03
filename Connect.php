f6681a7987e4dd3bc108fead26dbe9eb9a447418
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-10-03 19:10:24 +0200

    fix error

---
+++ Connect.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$servername = "localhost"; // Nome del server del database
+$username = "root";   // Nome utente del database
+$password = "root";      // Password del database
+$dbname = "db_edusogno";   // Nome del database
+
+// Creazione della connessione
+
+$conn = new mysqli($servername, $username, $password);
