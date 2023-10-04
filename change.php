fca0329e3a4c5ff06895698eff73c012597828cf
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-10-04 20:16:03 +0200

    add change

---
+++ change.php
@@ -0,0 +1,24 @@
+<?php
+// collegamento database mysql alla pagina index
+require_once 'Connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="./assets/js/script.js" defer></script>
+    <title>Edusogno</title>
+</head>
+
+<body>
+    <h1>Cambia qui la tua password</h1>
+
+</body>
+
+
+</html>
