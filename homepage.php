41e05451ef35d9ba21a65cddb929bd041560e21b
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-10-06 12:22:14 +0200

    .

--- homepage.php
+++ homepage.php
@@ -16,8 +16,8 @@ if (isset($_SESSION['nome'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="homepage.css">
-    <script src="./assets/js/script.js" defer></script>
     <title>HomePage</title>
+    <script src="./assets/js/script.js" defer></script>
 </head>
 
 <body>
