8e642aa1d03c8859015fbd8fe161d5a0709d6c6b
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-10-06 12:22:07 +0200

    .

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ require_once 'Connect.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <script src="./assets/js/script.js" defer></script>
     <title>Edusogno</title>
+    <script src="./assets/js/script.js" defer></script>
 </head>
 
 <body>
